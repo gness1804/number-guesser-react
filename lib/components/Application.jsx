2a7763c1562caae141c668d1426f7dd9d3bcbabd
gness1804 <graham.nessler@gmail.com>
2016-10-27 21:48:21 -0600

    finally got react working

---
+++ lib/components/Application.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Application extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render () {
+    return (
+      <div>Hello</div>
+    );
+  }
+} //end of Application
+
+module.exports = Application;
--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,28 @@ const path = require('path');
 module.exports = {
   entry: {
     main: ['babel-polyfill', './lib/index.js']
-      },
-    output: {
-      path: __dirname,
-      filename: "[name].bundle.js"
-      },
-      module: {
-        loaders: [
-          { test: /\.js$/, exclude: '/node_modules/', loader: 'babel-loader' },
-          { test: /\.css$/, loader: "style!css" },
-          { test: /\.scss$/, loader: "style!css!sass" }
-        ]
+  },
+  output: {
+    path: path.join(__dirname, 'public'),
+    publicPath: '/public/',
+    filename: '[name].bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react'],
         },
-        resolve: {
-          extensions: ['', '.js', '.jsx', '.json', '.scss', '.css']
-        }
-      };
+      },
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.scss$/, loader: 'style!css!sass' },
+    ],
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json', '.scss', '.css'],
+  },
+};
