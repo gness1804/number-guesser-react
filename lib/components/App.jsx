b70aa55401d06d70a8190df796a04a0c49f936bf
gness1804 <graham.nessler@gmail.com>
2016-10-27 20:09:58 -0600

    trying to set up react

---
+++ lib/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+//require any other jsx files needed
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render () {
+    return (
+      <div>Hi</div>
+    );
+  }
+} //end of App
+
+module.exports = App;
+//ReactDOM.render(<App title='' />, document.querySelector(''));
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,8 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    main: "./lib/index.js",
-    // test: "mocha!./test/index.js"
-    },
+    main: ['babel-polyfill', './lib/index.js']
+      },
     output: {
       path: __dirname,
       filename: "[name].bundle.js"
